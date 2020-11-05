4992bd8821cadeafb23f1d74e9870aaec6a082d1
pushpak4166 <pushpak4166@gmail.com>
2020-11-05 20:34:42 +0530

    modified

--- anomaly/models/model_loading.py
+++ anomaly/models/model_loading.py
@@ -1,8 +1,22 @@
 from keras.models import load_model
 import numpy as np
-model = load_model('anomaly_model_new.h5')
-samples = np.array([-97.72768685,30.55331562,27.240221,16,245,257,3.1477048,5.959532,6.654961,-0.015488088,86,1928.75,31,21.17647171,63,16.07843208])
+#loading the model
+
+model = load_model('anomaly_detection_model_final.h5')
+
+# 1st input data
+samples = np.array([[100,83,1952,19,5.26,57,0.231]])
 prediction = model.predict_classes(samples)
-print(prediction)
+print("For 1st input data: \n")
+print("Predicted class: ",prediction)
 pred = model.predict(samples)
-print(pred)
+print("Predicted prob: ",pred)
+print("\n")
+
+# 2nd input data
+samples2 = np.array([[96.11490583,109.8962564,3565.196403,66.58616576,57.66038318,12.05468241,0.761498981]])
+print("For 2nd input data: \n")
+prediction2 = model.predict_classes(samples2)
+print("Predicted class: ",prediction2)
+pred2 = model.predict(samples2)
+print("Predicted prob: ",pred2)
